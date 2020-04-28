b1448859b86afbaf340ed4057234c07762a358a9
dave <dave@DESKTOP-CRKS56L>
2020-04-27 21:36:59 -0700

    fixed the graphs

--- src/main/java/com/openhouseautomation/display/ReadingDataSourceServlet.java
+++ src/main/java/com/openhouseautomation/display/ReadingDataSourceServlet.java
@@ -9,7 +9,6 @@ import com.google.visualization.datasource.datatable.DataTable;
 import com.google.visualization.datasource.datatable.TableCell;
 import com.google.visualization.datasource.datatable.TableRow;
 import com.google.visualization.datasource.datatable.value.DateTimeValue;
-import com.google.visualization.datasource.datatable.value.Value;
 import com.google.visualization.datasource.datatable.value.ValueType;
 import com.google.visualization.datasource.query.Query;
 import com.openhouseautomation.Convutils;
@@ -23,6 +22,7 @@ import static com.openhouseautomation.OfyService.ofy;
 import com.openhouseautomation.model.DatastoreConfig;
 import com.openhouseautomation.model.Reading;
 import com.openhouseautomation.model.Sensor;
+import java.util.LinkedHashMap;
 
 import java.util.List;
 import java.util.logging.Level;
@@ -38,9 +38,9 @@ public class ReadingDataSourceServlet extends DataSourceServlet {
   private static final long serialVersionUID = 1L;
   private static final Logger log = Logger.getLogger(ReadingDataSourceServlet.class.getName());
 
-  int resolution = 5; // graph resolution in minutes
+  int resolution = 5; // graph resolution in minutes 
   int shortchartdays = Integer.parseInt(DatastoreConfig.getValueForKey("shortchartdays", "5"));
-  int blocks = 60 / resolution * 24 * shortchartdays; // blocks of time in graph (300k)
+  int blocks = 60 / resolution * 24 * shortchartdays; // blocks of time in graph (2k)
 
   @Override
   public DataTable generateDataTable(Query query, HttpServletRequest request) {
@@ -58,35 +58,37 @@ public class ReadingDataSourceServlet extends DataSourceServlet {
     }
     log.log(Level.INFO, "retrieve sensor classes took {0}ms", System.currentTimeMillis() - starttime);
     // add column descriptions
+    LinkedHashMap<Sensor, Iterable<Reading>> readingfuture = new LinkedHashMap();
     for (Sensor s : sensors) {
       data.addColumn(new ColumnDescription(Long.toString(s.getId()), ValueType.NUMBER, s.getName()));
+      Iterable<Reading> readings = ofy().load().type(Reading.class).ancestor(s).chunkAll().iterable();
+      readingfuture.put(s, readings);
     }
-
     // use the sensors to get the readings
     shortchartdays = Integer.parseInt(DatastoreConfig.getValueForKey("shortchartdays", "7"));
     DateTime cutoffdate = Convutils.getNewDateTime().minus(Period.days(shortchartdays));
     log.log(Level.INFO, "filling {0} blocks", blocks);
     Double[][] readingsz = new Double[sensors.size()][blocks + 10]; // fudge for ArrayIndexOutOfBoundsException
-    starttime = System.currentTimeMillis();
     // fill in the readings
-    for (int i = 0; i < sensors.size(); i++) {
-      Sensor s = (Sensor) sensors.get(i);
-      long starttime2 = System.currentTimeMillis();
-      Iterable<Reading> readings = ofy().load().type(Reading.class).ancestor(s).chunkAll().iterable();
+    int senscount=-1;
+    for (Sensor s : readingfuture.keySet()) {
+      senscount++;
       int readingcount = 0;
-      for (Reading reading : readings) {
+      long starttime2 = System.currentTimeMillis();
+      for (Reading reading : readingfuture.get(s)) {
         readingcount++;
         int blocknumber = (int) ((reading.getTimestamp().getMillis() - cutoffdate.getMillis()) / 1000 / 60 / resolution);
         if (blocknumber >= 0 && blocknumber < blocks) {
+          readingsz[senscount][blocknumber] = -1.0;
           try {
-            readingsz[i][blocknumber] = Double.parseDouble(reading.getValue());
+            readingsz[senscount][blocknumber] = Double.parseDouble(reading.getValue());
           } catch (java.lang.NumberFormatException e) {
             //log.log(Level.WARNING, "could not parse: {0}", reading);
-            readingsz[i][blocknumber] = null;
+            readingsz[senscount][blocknumber] = -1.0;
           }
         }
       }
-      log.log(Level.INFO, "parse and store {0} readings took {1}ms", new Object[]{readingcount, System.currentTimeMillis() - starttime2});
+      log.log(Level.INFO, "parse and store {0} readings from [1] took {2}ms as column#{3}", new Object[]{readingcount, s.getName(), System.currentTimeMillis() - starttime2, senscount});
     }
     log.log(Level.INFO, "time to get all readings: {0}ms", (System.currentTimeMillis() - starttime));
 
@@ -111,11 +113,15 @@ public class ReadingDataSourceServlet extends DataSourceServlet {
         for (int j = 0; j < sensors.size(); j++) {
           if (null == readingsz[j][i]) {
             // put in a blank
-            tr.addCell(new TableCell(Value.getNullValueFromValueType(data.getColumnDescription(j + 1).getType())));
-          } else {
-            // put in the actual value
-            tr.addCell(new TableCell(readingsz[j][i]));
+            //tr.addCell(new TableCell(Value.getNullValueFromValueType(data.getColumnDescription(j + 1).getType())));
+            if ( i > 0 ) {
+              readingsz[j][i] = readingsz[j][i-1];
+            } else {
+              readingsz[j][i] = -1.0;
+            }
           }
+          // put in the actual value
+          tr.addCell(new TableCell(readingsz[j][i]));
         }
         data.addRow(tr);
       }
